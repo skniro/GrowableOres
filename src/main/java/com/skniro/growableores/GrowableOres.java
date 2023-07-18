3dc6aa045dc3a3b14201ed75c78864025fe7f9c4
skniro <46392259+skniro@users.noreply.github.com>
2023-07-18 12:57:35 +0800

    1.19.4-1.0.1

--- src/main/java/com/skniro/growableores/GrowableOres.java
+++ src/main/java/com/skniro/growableores/GrowableOres.java
@@ -5,11 +5,6 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.registry.RegistryKey;
-import net.minecraft.registry.RegistryKeys;
-import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,14 +18,13 @@ public class GrowableOres implements ModInitializer {
 
 
 
-     public static final RegistryKey<ItemGroup> Growable_Ores_Group = RegistryKey.of(RegistryKeys.ITEM_GROUP, new Identifier(MOD_ID, "test_group"));
+     public static final ItemGroup Growable_Ores_Group = FabricItemGroup.builder(
+             new Identifier(MOD_ID, "test_group"))
+                     .icon(() -> new ItemStack(GrowableOresBlocks.Iron_Cane))
+                     .build();
 
     @Override
     public void onInitialize() {
-        Registry.register(Registries.ITEM_GROUP, Growable_Ores_Group, FabricItemGroup.builder()
-                .icon(() -> new ItemStack(GrowableOresBlocks.Iron_Cane))
-                .displayName(Text.translatable("itemGroup.growable_ores.test_group"))
-                .build()); // build() no longer registers by itself
         ModContent.registerItem();
         ModContent.registerBlock();
         ModContent.CreativeTab();

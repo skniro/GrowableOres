306493d23ba475313c1019a65331d9a921bf080a
skniro <46392259+skniro@users.noreply.github.com>
2024-06-11 12:53:07 +0800

    #15 Finish new structure. And finish fluix crystal texture. Add botanypots support.

---
+++ src/main/java/com/skniro/growableores/block/GrowableAdAstraOresBlocks.java
@@ -0,0 +1,45 @@
+package com.skniro.growableores.block;
+
+import com.skniro.growableores.GrowableOres;
+import com.skniro.growableores.util.GrowableOresItemGroups;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.MapColor;
+import net.minecraft.block.SugarCaneBlock;
+import net.minecraft.block.piston.PistonBehavior;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.Identifier;
+
+import java.util.logging.Logger;
+
+public class GrowableAdAstraOresBlocks {
+    //Ad Astra!
+    public static final Block AD_Ostrum_Cane =registerBlock("ad_mars_ore_ostrum_cane", new SugarCaneBlock(AbstractBlock.Settings.create().mapColor(MapColor.DARK_GREEN).noCollision().ticksRandomly().breakInstantly().sounds(BlockSoundGroup.GRASS).pistonBehavior(PistonBehavior.DESTROY)),GrowableOresItemGroups.Growable_Ores_Group);
+    public static final Block AD_Ice_Shard_Cane =registerBlock("ad_moon_ice_shard_ore_cane", new SugarCaneBlock(AbstractBlock.Settings.create().mapColor(MapColor.DARK_GREEN).noCollision().ticksRandomly().breakInstantly().sounds(BlockSoundGroup.GRASS).pistonBehavior(PistonBehavior.DESTROY)),GrowableOresItemGroups.Growable_Ores_Group);
+    public static final Block AD_Cheese_Cane =registerBlock("ad_moon_ore_cheese_cane", new SugarCaneBlock(AbstractBlock.Settings.create().mapColor(MapColor.DARK_GREEN).noCollision().ticksRandomly().breakInstantly().sounds(BlockSoundGroup.GRASS).pistonBehavior(PistonBehavior.DESTROY)),GrowableOresItemGroups.Growable_Ores_Group);
+    public static final Block AD_Desh_Cane =registerBlock("ad_moon_ore_desh_cane", new SugarCaneBlock(AbstractBlock.Settings.create().mapColor(MapColor.DARK_GREEN).noCollision().ticksRandomly().breakInstantly().sounds(BlockSoundGroup.GRASS).pistonBehavior(PistonBehavior.DESTROY)),GrowableOresItemGroups.Growable_Ores_Group);
+    public static final Block AD_Calorite_Cane =registerBlock("ad_venus_calorite_ore_cane", new SugarCaneBlock(AbstractBlock.Settings.create().mapColor(MapColor.DARK_GREEN).noCollision().ticksRandomly().breakInstantly().sounds(BlockSoundGroup.GRASS).pistonBehavior(PistonBehavior.DESTROY)),GrowableOresItemGroups.Growable_Ores_Group);
+
+
+
+    private static Block registerBlock(String name, Block block, RegistryKey<ItemGroup> tab) {
+        registerBlockItem(name, block, tab);
+        return Registry.register(Registries.BLOCK, new Identifier(GrowableOres.MOD_ID, name), block);
+    }
+
+    private static Item registerBlockItem(String name, Block block, RegistryKey<ItemGroup> tab) {
+        return Registry.register(Registries.ITEM, new Identifier(GrowableOres.MOD_ID, name),
+                new BlockItem(block, new FabricItemSettings()));
+    }
+    public static void registerModBlocks(){
+        Logger.getLogger("register mod blocks" + GrowableOres.MOD_ID);
+    }
+}
+

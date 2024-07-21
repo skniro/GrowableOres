ef802cf4bb3eee167476c531bd7a25084da7735a
skniro <46392259+skniro@users.noreply.github.com>
2024-07-21 13:38:52 +0800

    Sync with 1.21

---
+++ Fabric/src/main/java/com/skniro/growableores/block/GrowableBetterNetherOresBlocks.java
@@ -0,0 +1,41 @@
+package com.skniro.growableores.block;
+
+import com.skniro.growableores.GrowableOres;
+import com.skniro.growableores.util.GrowableOresItemGroups;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.MapColor;
+import net.minecraft.block.SugarCaneBlock;
+import net.minecraft.block.piston.PistonBehavior;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.Identifier;
+
+import java.util.logging.Logger;
+
+public class GrowableBetterNetherOresBlocks {
+    //BetterNether
+    public static final Block BN_Cincinnasite_Cane =registerBlock("bn_cincinnasite_cane", new SugarCaneBlock(AbstractBlock.Settings.create().mapColor(MapColor.DARK_GREEN).noCollision().ticksRandomly().breakInstantly().sounds(BlockSoundGroup.GRASS).pistonBehavior(PistonBehavior.DESTROY)), GrowableOresItemGroups.Growable_Ores_Group);
+    public static final Block BN_Nether_Ruby_Cane =registerBlock("bn_nether_ruby_cane", new SugarCaneBlock(AbstractBlock.Settings.create().mapColor(MapColor.DARK_GREEN).noCollision().ticksRandomly().breakInstantly().sounds(BlockSoundGroup.GRASS).pistonBehavior(PistonBehavior.DESTROY)), GrowableOresItemGroups.Growable_Ores_Group);
+
+
+
+    private static Block registerBlock(String name, Block block, RegistryKey<ItemGroup> tab) {
+        registerBlockItem(name, block, tab);
+        return Registry.register(Registries.BLOCK, Identifier.of(GrowableOres.MOD_ID, name), block);
+    }
+
+    private static Item registerBlockItem(String name, Block block, RegistryKey<ItemGroup> tab) {
+        return Registry.register(Registries.ITEM, Identifier.of(GrowableOres.MOD_ID, name),
+                new BlockItem(block, new Item.Settings()));
+    }
+    public static void registerModBlocks(){
+        Logger.getLogger("register mod blocks" + GrowableOres.MOD_ID);
+    }
+}
+

789be32f4cbe55a402e7e2370a875441cc49d9a9
skniro <46392259+skniro@users.noreply.github.com>
2024-11-30 21:56:17 +0800

    prepare for 3.0.0

--- src/main/java/com/skniro/growableores/conifg/GrowableOresConfig.java
+++
@@ -1,6 +0,0 @@
-package com.skniro.growableores.conifg;
-
-public class GrowableOresConfig {
-    @Config(config = "growableores", category = "function", key = "All Item Mode",comment = "Disable All Item Mode.")
-    public static boolean All_Item_Mode = true;
-}

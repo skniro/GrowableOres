e0fa53933148f400a0a252b84328895eb09806c1
skniro <46392259+skniro@users.noreply.github.com>
2025-01-08 18:25:54 +0800

    Fix block render.(Forge) Fix register.(Fabric)

---
+++ Forge/src/main/java/com/skniro/growableores/client/ClientHelper.java
@@ -0,0 +1,33 @@
+package com.skniro.growableores.client;
+
+
+import com.skniro.growableores.GrowableOres;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RegisterParticleProvidersEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.registries.ForgeRegistries;
+
+@Mod.EventBusSubscriber(modid = GrowableOres.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public class ClientHelper {
+
+    @SubscribeEvent
+    public static void onClientSetup(FMLClientSetupEvent event) {
+        event.enqueueWork(() -> {
+            ForgeRegistries.BLOCKS.forEach(block -> {
+                ResourceLocation blockId = ForgeRegistries.BLOCKS.getKey(block);
+                if (blockId != null && blockId.getNamespace().equals(GrowableOres.MOD_ID)) {
+                    ItemBlockRenderTypes.setRenderLayer(block, RenderType.cutout());
+                }
+            });
+        });
+    }
+
+    @SubscribeEvent
+    public static void onParticleFactoryRegistration(RegisterParticleProvidersEvent event) {
+    }
+}
